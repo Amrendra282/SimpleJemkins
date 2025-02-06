0e07a63a245a6b356134c855ba6537ddb07680e6
Amrendra282 <amrendramishra54@gmail.com>
2025-02-06 13:56:24 +0530

    onPollSCM committed

---
+++ SimpleJenkins/src/test/java/Demo/OnPollSCMTest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPollSCMTest {
+	@Test
+	public void test() {
+		Reporter.log("OnPollSCM class added", true);
+	}
+
+}

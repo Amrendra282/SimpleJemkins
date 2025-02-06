080b53c2ce03f72abcb2487e366ab683dc4b9d3a
Amrendra282 <amrendramishra54@gmail.com>
2025-02-06 13:09:39 +0530

    Simple Jenkins class added.

---
+++ SimpleJenkins/src/test/java/Demo/JenkinsTest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void sample() {
+		Reporter.log("JenkinsTest class executed", true);
+	}
+
+}
